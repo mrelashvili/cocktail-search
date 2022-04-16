305922d2d0c952716d6f532f9c1c61670bdb9032
mrelashvili <mrmrela5@gmail.com>
2022-04-17 00:29:56 +0400

    cocktails-v1

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi tempore
+      nesciunt omnis, minima inventore corporis, aspernatur nihil impedit saepe
+      veniam totam ipsum deserunt atque, dignissimos harum? Reprehenderit quam
+      sunt ex voluptatem officia tempora, aliquid at corrupti non veniam nisi
+      asperiores?
+    </section>
+  );
+};
+
+export default About;
